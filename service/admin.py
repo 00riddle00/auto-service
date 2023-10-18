7a449435cda596ed2a23f0945ab8f6cc04477221
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-18 17:35:23 +0300

    [Problem P27] Configure an Admin page according to our Wiki, topic no. 27.

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car, CarModel, Order, OrderLine, Service
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["license_plate_no", "vin_code", "car_model", "client_name"]
+    list_filter = ["client_name", "car_model__make", "car_model__model"]
+    search_fields = ["license_plate_no", "vin_code"]
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["name", "price"]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("date", "car", "total_price")
+    list_editable = "total_price"
+    inlines = ["OrderLineInline"]
+
+
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    readonly_fields = ["id"]
+    can_delete = False
+    extra = 0
+
+
+admin.site.register(Car)
+admin.site.register(CarModel)
+admin.site.register(Service)
+admin.site.register(Order)
+admin.site.register(OrderLine)
