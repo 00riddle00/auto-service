79e23c75d8bfb3b653063253ddc298f1a54f3f0d
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-18 18:10:05 +0300

    [Problem 28] Add an index page for Car Service application (using templates).

--- service/views.py
+++ service/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Car, CarModel, Order, Service
 
 
 def index(request):
-    return HttpResponse("Hello, World! Welcome to our Auto Service!")
+    num_cars = Car.objects.all().count()
+    num_models = CarModel.objects.all().count()
+    num_services = Service.objects.all().count()
+    num_orders = Order.objects.all().count()
+
+    context = {
+        "num_cars": num_cars,
+        "num_models": num_models,
+        "num_services": num_services,
+        "num_orders": num_orders,
+    }
+
+    return render(request, "index.html", context=context)
