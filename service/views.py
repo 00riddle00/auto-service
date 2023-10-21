795a89efaeea937d34c3b49fd6a9a1e4837e3acb
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-21 22:18:30 +0300

    [Problem P28] Show only completed orders (status field is used for that).

--- service/views.py
+++ service/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 
-from .models import Car, CarModel, Order, Service
+from .models import Car, Order, Service
 
 
 def index(request):
     num_cars = Car.objects.all().count()
-    num_models = CarModel.objects.all().count()
     num_services = Service.objects.all().count()
-    num_orders = Order.objects.all().count()
+    num_orders = Order.objects.filter(status__exact="C").count()
 
     context = {
         "num_cars": num_cars,
-        "num_models": num_models,
         "num_services": num_services,
         "num_orders": num_orders,
     }
