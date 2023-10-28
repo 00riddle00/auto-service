f5a4c25b16c687c8e0aa3ccf312029b5addbfad4
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-28 20:46:53 +0300

    [Problem P32] Add a default TinyMCE configuration to Django settings.

--- auto_service/settings.py
+++ auto_service/settings.py
@@ -146,3 +146,33 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = EMAIL_HOST_USER_SECRET
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD_SECRET
+
+# TinyMCE for WYSIWYG editing
+
+TINYMCE_DEFAULT_CONFIG = {
+    "height": 360,
+    "width": 1120,
+    "cleanup_on_startup": True,
+    "custom_undo_redo_levels": 20,
+    "selector": "textarea",
+    "plugins": """
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            """,
+    "toolbar1": """
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            """,
+    "toolbar2": """
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            """,
+    "contextmenu": "formats | link image",
+    "menubar": True,
+    "statusbar": True,
+}
