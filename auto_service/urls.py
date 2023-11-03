a4f7f1036f2a9367dde6cb05a05001fd8f40ac65
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-11-03 21:29:37 +0200

    [Problem P34] Drop "service/" subpath in URLs.

--- auto_service/urls.py
+++ auto_service/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 from service import views as service_views
 
 urlpatterns = (
@@ -32,8 +31,7 @@ urlpatterns = (
             name="register-complete",
         ),
         path("tinymce/", include("tinymce.urls")),
-        path("service/", include("service.urls")),
-        path("", RedirectView.as_view(url="service/", permanent=True)),
+        path("", include("service.urls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
