8fb4e10855b294141ee892e58be1b165a3f5d87c
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-11-07 13:47:36 +0200

    [Problem P37] Add a few simple tests for URLs.

---
+++ service/tests/test_urls.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+from service.views import OrderDetailView, OrderListView, cars
+
+
+class TestUrls(TestCase):
+    def test_cars_url_is_resolved(self):
+        url = reverse("cars")
+        self.assertEqual(resolve(url).func, cars)
+
+    def test_orders_url_is_resolved(self):
+        url = reverse("orders")
+        self.assertEqual(resolve(url).func.view_class, OrderListView)
+
+    def test_order_details_url_is_resolved(self):
+        url = reverse("order-details", args=[1])
+        self.assertEqual(resolve(url).func.view_class, OrderDetailView)
