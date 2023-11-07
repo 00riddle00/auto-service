dc3b007f282c6eb4edc8a1935186341a097e8848
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-11-07 13:44:58 +0200

    [Problem P37] Add a few simple tests for "user-orders" view.

--- service/tests/test_views.py
+++ service/tests/test_views.py
@@ -1,7 +1,10 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
-from service.models import Service
+from service.models import Car, CarModel, Order, Service
 
 
 class TestServiceListView(TestCase):
@@ -30,3 +33,53 @@ class TestServiceListView(TestCase):
         self.assertTrue("is_paginated" in response.context)
         self.assertTrue(response.context["is_paginated"])
         self.assertEqual(len(response.context["services"]), 5)
+
+
+class TestUserOrderListView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(
+            username="john_doe", password="VerySecretPassword123"
+        )
+
+        CarModel.objects.create(
+            make="Ford",
+            model="Mondeo",
+            year=2009,
+            engine_type="2000 cm³, 107kW",
+            fuel_type="Gasoline",
+        )
+
+        Car.objects.create(
+            license_plate_no="FMC516",
+            vin_code="WAUNE78P28A492843",
+            car_model=CarModel.objects.get(id=1),
+            client_name="John Doe",
+        )
+
+        Order.objects.create(
+            car=Car.objects.get(id=1),
+            user=test_user,
+            deadline=datetime.date.today() + datetime.timedelta(days=3),
+        )
+
+    def test_logged_in_uses_correct_template(self):
+        self.client.login(
+            username="john_doe", password="VerySecretPassword123"
+        )
+        response = self.client.get(reverse("user-orders"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.context["user"]), "john_doe")
+        self.assertTemplateUsed(response, template_name="user_orders.html")
+
+    def test_user_order_in_list(self):
+        self.client.login(
+            username="john_doe", password="VerySecretPassword123"
+        )
+        response = self.client.get(reverse("user-orders"))
+        self.assertEqual(str(response.context["user"]), "john_doe")
+        self.assertTrue("user_orders" in response.context)
+        self.assertEqual(len(response.context["user_orders"]), 1)
+        self.assertEqual(
+            response.context["user_orders"][0].car.license_plate_no, "FMC516"
+        )
