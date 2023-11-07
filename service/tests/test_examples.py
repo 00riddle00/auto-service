a404426db7d5072430c27b523853f17cc83ed32f
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-11-07 13:24:03 +0200

    [Problem P37] Add example tests which show the testing flow.

---
+++ service/tests/test_examples.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+
+
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print(
+            "\nMethod setUpTestData is run once at the beginning of the test "
+            "case for class level setup, it creates unmodified data for all "
+            "remaining test methods."
+        )
+        pass
+
+    def setUp(self):
+        print(
+            "\nMethod setUp is run once before every test method to setup "
+            "clean data."
+        )
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        # Will be marked as successful (i.e. test passed):
+        # a dot (.) will appear
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        # Will be marked as failed:
+        # an (F) will appear
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        # Will be marked as successful (test passed).
+        # a dot (.) will appear
+        self.assertEqual(1 + 1, 2)
